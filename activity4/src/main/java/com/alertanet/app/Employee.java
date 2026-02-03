8a2c980d50decd8a45982ae1a78e8aa1a0fcc498
RASERUI <russellroy.ignacio@lorma.edu>
2026-02-03 15:23:04 +0800

    Add files via upload

---
+++ activity4/src/main/java/com/alertanet/app/Employee.java
@@ -0,0 +1,17 @@
+package com.alertanet.app;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+}
+    
+    
